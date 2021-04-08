e9db6e4524b0b873f362143e5a914749fc72404f
James Henstridge <james@jamesh.id.au>
2021-04-08 17:57:29 +0800

    ubuntu-desktop-session: don't set DISPLAY: this confuses gnome-shell

--- ubuntu-desktop-session/scripts/session-wrapper.sh
+++ ubuntu-desktop-session/scripts/session-wrapper.sh
@@ -8,9 +8,7 @@ if [ -f /snap/snapd/current/etc/profile.d/apps-bin-path.sh ]; then
     source /snap/snapd/current/etc/profile.d/apps-bin-path.sh
 fi
 
-# The confined desktop environment will probably be on :0, so this is
-# one less thing to fix up after login.
-export DISPLAY=:0
+export XDG_CURRENT_DESKTOP=ubuntu:GNOME
 
 dbus-update-activation-environment  --systemd --all
 
