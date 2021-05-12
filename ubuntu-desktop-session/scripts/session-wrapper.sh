0d2d9a0ea2a850c95d1ee5a01b95f5db8866387d
James Henstridge <james@jamesh.id.au>
2021-05-12 13:05:05 +0800

    ubuntu-desktop-session: try to configure DISPLAY and Xauthority from session-wrapper.sh

--- ubuntu-desktop-session/scripts/session-wrapper.sh
+++ ubuntu-desktop-session/scripts/session-wrapper.sh
@@ -10,6 +10,25 @@ fi
 
 export XDG_CURRENT_DESKTOP=ubuntu:GNOME
 
-dbus-update-activation-environment  --systemd --all
+dbus-update-activation-environment --systemd --all
+
+# Don't set this in our own environment, since it will make
+# gnome-session believe it is running in X mode
+dbus-update-activation-environment --systemd DISPLAY=:0
+
+# Set up a background task to wait for gnome-session to create its
+# Xauthority file, and copy it to a location snaps will be able to
+# see.
+function fixup_xauthority() {
+    while :; do
+        sleep 1s
+        xauth_file="$(ls -1t /run/user/$(id -u)/snap.ubuntu-desktop-session/.mutter-Xwaylandauth.* | head -n1)"
+        if [ -f "$xauth_file" ]; then
+            cp "$xauth_file" /run/user/$(id -u)/.Xauthority
+            return
+        fi
+    done
+}
+fixup_xauthority &
 
 exec /snap/bin/ubuntu-desktop-session
