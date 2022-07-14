16fb85cc76cf7ca0003a816e17390d1ab078f164
James Henstridge <james@jamesh.id.au>
2022-07-14 10:28:19 +0800

    ubuntu-desktop-session: add back code to create /tmp/.X11-unix
    It looks like gnome-shell's socket dir creation is a bit buggy: due to
    umask, it creates it with permissions that it later rejects.

--- ubuntu-desktop-session/scripts/run-session.sh
+++ ubuntu-desktop-session/scripts/run-session.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+# Ensure socket directory exists and has the right permissions
+mkdir -p /tmp/.X11-unix
+chmod 01777 /tmp/.X11-unix
+
 export GNOME_SHELL_SESSION_MODE=ubuntu
 exec /usr/bin/gnome-session --session=ubuntu
