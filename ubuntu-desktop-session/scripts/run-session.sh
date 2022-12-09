a2af7dc17f890284920298cef648819cbdbafd56
Robert Ancell <robert.ancell@canonical.com>
2022-12-09 15:28:03 +1300

    Create XDG_RUNTIME_DIR on startup

--- ubuntu-desktop-session/scripts/run-session.sh
+++ ubuntu-desktop-session/scripts/run-session.sh
@@ -4,5 +4,8 @@
 mkdir -p /tmp/.X11-unix
 chmod 01777 /tmp/.X11-unix
 
+# Create the runtime directory
+mkdir -p --mode=700 $XDG_RUNTIME_DIR
+
 export GNOME_SHELL_SESSION_MODE=ubuntu
 exec /usr/bin/gnome-session --session=ubuntu
