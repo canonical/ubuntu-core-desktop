170800c213e931afc92b1283063aa021bb8fead8
Ken VanDine <ken.vandine@canonical.com>
2023-09-20 15:49:53 -0400

    Don't try to unmount too quickly

--- image/tweak.sh
+++ image/tweak.sh
@@ -3,6 +3,6 @@
 mkdir -p $PWD/output/mnt
 mount -o loop,offset=$(expr 512 \* $(fdisk -l output/installer-amd64.img |grep img3 | awk '{print $2}')) output/installer-amd64.img $PWD/output/mnt
 mkdir -p $PWD/output/mnt/cdrom/casper
-mv $PWD/output/mnt/{pc.img.xz,install-sources.yaml} $PWD/output/mnt/cdrom/casper/
+mv $PWD/output/mnt/{pc.img.xz,install-sources.yaml} $PWD/output/mnt/cdrom/casper/ && sleep 5
 umount $PWD/output/mnt
 rmdir $PWD/output/mnt
