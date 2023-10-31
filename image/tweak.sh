9650e900a606f6429d6e58d148ff9036ead789ca
Ken VanDine <ken.vandine@canonical.com>
2023-10-31 17:37:43 +0200

    Dropped tweak.sh and utiliize make-dirs in ubuntu-image, requires 3.0+snap18 or later

--- image/tweak.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-mkdir -p $PWD/output/mnt
-mount -o loop,offset=$(expr 512 \* $(fdisk -l output/ubuntu-core-desktop-22-amd64.img |grep img3 | awk '{print $2}')) output/ubuntu-core-desktop-22-amd64.img $PWD/output/mnt
-mkdir -p $PWD/output/mnt/cdrom/casper
-mv $PWD/output/mnt/{pc.img.xz,install-sources.yaml} $PWD/output/mnt/cdrom/casper/ && sync && sleep 30
-umount $PWD/output/mnt
-rmdir $PWD/output/mnt
