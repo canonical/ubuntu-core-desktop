15edd5e4502e07b45a4c3eac276ba9ab7d1776b0
Ken VanDine <ken.vandine@canonical.com>
2023-10-31 03:15:08 +0200

    Updated installer image file

--- image/tweak.sh
+++ image/tweak.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 mkdir -p $PWD/output/mnt
-mount -o loop,offset=$(expr 512 \* $(fdisk -l output/installer-amd64.img |grep img3 | awk '{print $2}')) output/installer-amd64.img $PWD/output/mnt
+mount -o loop,offset=$(expr 512 \* $(fdisk -l output/ubuntu-core-desktop-22-amd64.img |grep img3 | awk '{print $2}')) output/ubuntu-core-desktop-22-amd64.img $PWD/output/mnt
 mkdir -p $PWD/output/mnt/cdrom/casper
 mv $PWD/output/mnt/{pc.img.xz,install-sources.yaml} $PWD/output/mnt/cdrom/casper/ && sync && sleep 30
 umount $PWD/output/mnt
